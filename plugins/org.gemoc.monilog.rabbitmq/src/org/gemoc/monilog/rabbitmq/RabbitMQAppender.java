8c5abd7e9270bead4f0917d9b28a2d5acf98e2be
LEROY Dorian <dorian.leroy@irisa.fr>
2021-06-17 14:29:02 +0200

    improved rabbitmq appender

--- plugins/org.gemoc.monilog.rabbitmq/src/org/gemoc/monilog/rabbitmq/RabbitMQAppender.java
+++ plugins/org.gemoc.monilog.rabbitmq/src/org/gemoc/monilog/rabbitmq/RabbitMQAppender.java
@@ -2,38 +2,60 @@ package org.gemoc.monilog.rabbitmq;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.gemoc.monilog.api.IMoniLogAppender;
 
-import com.google.gson.Gson;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class RabbitMQAppender implements IMoniLogAppender {
 
-	final Gson gson = new Gson();
-	final ConnectionFactory factory = new ConnectionFactory();
+	private final ConnectionFactory factory = new ConnectionFactory();
+	private final BlockingQueue<Object[]> toSend = new LinkedBlockingQueue<>();
+	
+	private Thread currentSenderThread = null;
 	
 	public RabbitMQAppender() {
-		
+		factory.setHost("localhost");
 	}
 	
 	@Override
 	public void call(Object... args) {
-		if (args.length == 3) {
-			factory.setHost(args[2].toString());
-		} else {
-			factory.setHost("localhost");
+		if (getCurrentSenderThread() == null) {
+			startSending();
 		}
-		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-			channel.queueDeclare(args[1].toString(), false, false, false, null);
-			channel.basicPublish("", args[1].toString(), null, args[0].toString().getBytes(StandardCharsets.UTF_8));
-//			channel.basicPublish("", args[1].toString(), null, gson.toJson(args[0]).getBytes(StandardCharsets.UTF_8));
-		} catch (IOException | TimeoutException e) {
-			e.printStackTrace();
-		}
+		toSend.add(args);
+	}
+	
+	private synchronized void startSending() {
+		final Thread t = new Thread(() -> {
+			try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+				Object[] current = toSend.poll(1000, TimeUnit.MILLISECONDS);
+				while (current != null) {
+					channel.queueDeclare(current[1].toString(), false, false, false, null);
+					channel.basicPublish("", current[1].toString(), null, current[0].toString().getBytes(StandardCharsets.UTF_8));
+					current = toSend.poll(1000, TimeUnit.MILLISECONDS);
+				}
+			} catch (IOException | TimeoutException | InterruptedException e) {
+				e.printStackTrace();
+			}
+			setCurrentSenderThread(null);
+		});
+		this.currentSenderThread = t;
+		t.start();
+	}
+	
+	private synchronized void setCurrentSenderThread(Thread currentSenderThread) {
+		this.currentSenderThread = currentSenderThread;
+	}
+	
+	private synchronized Thread getCurrentSenderThread() {
+		return this.currentSenderThread;
 	}
 
 	@Override
@@ -43,7 +65,7 @@ public class RabbitMQAppender implements IMoniLogAppender {
 
 	@Override
 	public int getNbMaxArgs() {
-		return 3;
+		return 2;
 	}
 
 	@Override
@@ -51,7 +73,6 @@ public class RabbitMQAppender implements IMoniLogAppender {
 		switch (argIndex) {
 		case 0: return Object.class;
 		case 1: return String.class;
-		case 2: return String.class;
 		}
 		return null;
 	}
